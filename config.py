71ded3d334ddf68cd7f80e711cd49efdb1db2e61
defff666 <soulpower13666@gmail.com>
2025-05-15 18:35:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
-WEB_APP_URL = os.getenv("WEB_APP_URL")
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+DATABASE_URL = os.getenv('DATABASE_URL')
+WEB_APP_URL = os.getenv('WEB_APP_URL')
+ADMIN_IDS = [int(id) for id in os.getenv('ADMIN_IDS', '').split(',') if id]
