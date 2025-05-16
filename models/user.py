5e9fb06c84c2c91ca517f200919d865bb553dc3d
defff666 <soulpower13666@gmail.com>
2025-05-16 10:31:42 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,23 +1,16 @@
 from dataclasses import dataclass
-from typing import List, Optional
 
 @dataclass
 class User:
-    id: int
+    user_id: int
     nickname: str
     age: int
     country: str
     city: str
     gender: str
-    interests: str
-    photo: Optional[str] = None
-    coins: int = 0
-    likes: List[str] = None
-    matches: List[str] = None
+    interests: list
+    photo_url: str = None
+    coins: int = 10
     blocked: bool = False
-
-    def __post_init__(self):
-        if self.likes is None:
-            self.likes = []
-        if self.matches is None:
-            self.matches = []
+    likes: list = None
+    matches: list = None
