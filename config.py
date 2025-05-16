f4ffbd4a20b76e067b6de6af11f43c964199db30
defff666 <soulpower13666@gmail.com>
2025-05-16 10:52:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-DATABASE_URL = os.getenv('DATABASE_URL')
-WEB_APP_URL = os.getenv('WEB_APP_URL')
-ADMIN_IDS = [int(id) for id in os.getenv('ADMIN_IDS', '').split(',') if id]
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+ADMIN_IDS = [int(id) for id in os.getenv("ADMIN_IDS", "").split(",") if id.strip().isdigit()]
+WEBAPP_URL = os.getenv("WEBAPP_URL", "https://cryptodivebot.onrender.com")
