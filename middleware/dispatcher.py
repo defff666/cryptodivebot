070b1f6567cc5527d81b921043b070eb415bb6f2
defff666 <soulpower13666@gmail.com>
2025-05-16 10:32:05 +0300

    Update dispatcher.py

--- middleware/dispatcher.py
+++ middleware/dispatcher.py
@@ -1,13 +1,11 @@
 from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject
-from typing import Callable, Dict, Any, Awaitable
 
 class DispatcherMiddleware(BaseMiddleware):
-    async def __call__(
-        self,
-        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-        event: TelegramObject,
-        data: Dict[str, Any]
-    ) -> Any:
-        data["dp"] = data.get("dp")
+    def __init__(self, dp):
+        self.dp = dp
+        super().__init__()
+
+    async def __call__(self, handler, event: TelegramObject, data: dict):
+        data["dp"] = self.dp
         return await handler(event, data)
